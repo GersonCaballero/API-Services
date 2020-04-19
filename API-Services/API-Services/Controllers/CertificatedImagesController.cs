14f5c4b63a030c2144897335787ccd87d7b4d78c
GersonCaballero <51335862+GersonCaballero@users.noreply.github.com>
2020-04-18 22:29:16 -0600

    Controllers
    Create Controllers

---
+++ API-Services/API-Services/Controllers/CertificatedImagesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API_Services.Models;
+
+namespace API_Services.Controllers
+{
+    public class CertificatedImagesController : ApiController
+    {
+        private APIServicesContext db = new APIServicesContext();
+
+        // GET: api/CertificatedImages
+        public IQueryable<CertificatedImage> GetCertificatedImages()
+        {
+            return db.CertificatedImages;
+        }
+
+        // GET: api/CertificatedImages/5
+        [ResponseType(typeof(CertificatedImage))]
+        public IHttpActionResult GetCertificatedImage(int id)
+        {
+            CertificatedImage certificatedImage = db.CertificatedImages.Find(id);
+            if (certificatedImage == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(certificatedImage);
+        }
+
+        // PUT: api/CertificatedImages/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCertificatedImage(int id, CertificatedImage certificatedImage)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != certificatedImage.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(certificatedImage).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CertificatedImageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CertificatedImages
+        [ResponseType(typeof(CertificatedImage))]
+        public IHttpActionResult PostCertificatedImage(CertificatedImage certificatedImage)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CertificatedImages.Add(certificatedImage);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = certificatedImage.Id }, certificatedImage);
+        }
+
+        // DELETE: api/CertificatedImages/5
+        [ResponseType(typeof(CertificatedImage))]
+        public IHttpActionResult DeleteCertificatedImage(int id)
+        {
+            CertificatedImage certificatedImage = db.CertificatedImages.Find(id);
+            if (certificatedImage == null)
+            {
+                return NotFound();
+            }
+
+            db.CertificatedImages.Remove(certificatedImage);
+            db.SaveChanges();
+
+            return Ok(certificatedImage);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CertificatedImageExists(int id)
+        {
+            return db.CertificatedImages.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ API-Services/API-Services/Controllers/FinalUsersController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API_Services.Models;
+
+namespace API_Services.Controllers
+{
+    public class FinalUsersController : ApiController
+    {
+        private APIServicesContext db = new APIServicesContext();
+
+        // GET: api/FinalUsers
+        public IQueryable<FinalUser> GetFinalUsers()
+        {
+            return db.FinalUsers;
+        }
+
+        // GET: api/FinalUsers/5
+        [ResponseType(typeof(FinalUser))]
+        public IHttpActionResult GetFinalUser(int id)
+        {
+            FinalUser finalUser = db.FinalUsers.Find(id);
+            if (finalUser == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(finalUser);
+        }
+
+        // PUT: api/FinalUsers/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutFinalUser(int id, FinalUser finalUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != finalUser.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(finalUser).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FinalUserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/FinalUsers
+        [ResponseType(typeof(FinalUser))]
+        public IHttpActionResult PostFinalUser(FinalUser finalUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.FinalUsers.Add(finalUser);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = finalUser.Id }, finalUser);
+        }
+
+        // DELETE: api/FinalUsers/5
+        [ResponseType(typeof(FinalUser))]
+        public IHttpActionResult DeleteFinalUser(int id)
+        {
+            FinalUser finalUser = db.FinalUsers.Find(id);
+            if (finalUser == null)
+            {
+                return NotFound();
+            }
+
+            db.FinalUsers.Remove(finalUser);
+            db.SaveChanges();
+
+            return Ok(finalUser);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool FinalUserExists(int id)
+        {
+            return db.FinalUsers.Count(e => e.Id == id) > 0;
+        }
+    }
+}
